951d8076ae6095bb47c82d73c399d08c081ac768
DinavHel <gonzalezgomez.adriangg@gmail.com>
2024-12-26 21:43:06 +0100

    Extract links from links and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import *
 
 
@@ -21,3 +22,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+
+def extract_markdown_images(text):
+    image_list = []
+    images = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    for image in images:
+        alt = image[(image.find('[') + 1):(image.find(']'))]
+        src = image[(image.find('(') + 1):(image.find(')'))]
+        image_list.append((alt, src))
+    return image_list
+
+
+def extract_markdown_links(text):
+    link_list = []
+    links = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    for link in links:
+        link_list.append(link)
+    return link_list
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -81,3 +81,17 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+
+    def text_extract_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        actual = extract_markdown_images(text)
+
+
+    def test_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        actual = extract_markdown_links(text)
+        self.assertEqual(expected, actual)
