0f0efcb55d521b3a2b6c002c565caca05a3c0efe
DinavHel <gonzalezgomez.adriangg@gmail.com>
2025-01-05 13:43:38 +0100

    Add functionality to split blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -10,3 +10,27 @@ def markdown_to_blocks(markdown: str) -> list:
         blocks.append(block.strip())
 
     return blocks
+
+
+def block_to_block_type(block: str) -> str:
+    heading_pattern = r"#{1,6} "
+    heading_match = re.match(heading_pattern, block)
+    if heading_match is not None:
+        return "heading"
+    
+    code_pattern = r"\`{3}"
+    code_match = re.findall(code_pattern, block)
+    if len(code_match) == 2:
+        return "code"
+
+    if re.match(r"[(\-,\*) .*]+", block) is not None:
+        return "unordered_list"
+
+    quote_pattern = r"[>.*]+"
+    if re.match(quote_pattern, block) is not None:
+        return "quote"
+
+    if re.match(r"[\d+\. .*]+", block) is not None:
+        return "ordered_list"
+
+    return "paragraph"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -25,3 +25,77 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 
         actual = markdown_to_blocks(markdown)
         self.assertEqual(expected, actual)
+    
+
+    def test_block_to_header_type(self):
+        heading = "# Heading 1"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+
+        heading = "## Heading 2"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+
+        heading = "### Heading 3"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+
+        heading = "#### Heading 4"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+
+        heading = "##### Heading 5"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+
+        heading = "###### Heading 6"
+        expected = "heading"
+        actual = block_to_block_type(heading)
+        self.assertEqual(expected, actual)
+    
+
+    def test_block_to_code_type(self):
+        c_code = """```
+int main(void)
+{
+    return 0;
+}
+```"""
+        expected = "code"
+        actual = block_to_block_type(c_code)
+        self.assertEqual(expected, actual)
+
+
+    def test_block_to_quote_type(self):
+        quote = """> We have to reinvent the wheel every once every once in a while, not because we need a lot of wheels;
+> but because we need a lot of inventors.
+> -- Bruce Joyce"""
+        expected = "quote"
+        actual = block_to_block_type(quote)
+        self.assertEqual(expected, actual)
+
+
+    def test_block_to_unordered_list_type(self):
+        unordered_list = """* Element 1
+* Element 2
+- Element 3
+- Element 4"""
+        expected = "unordered_list"
+        actual = block_to_block_type(unordered_list)
+        self.assertEqual(expected, actual)
+
+
+    def test_block_to_ordered_list_type(self):
+        unordered_list = """1. Element a
+2. Element b
+3. Element c
+4. Element d"""
+        expected = "ordered_list"
+        actual = block_to_block_type(unordered_list)
+        self.assertEqual(expected, actual)
+
